1a96eba1cf9cadac68b7789c5b75eb84d78fb57b
masterUNG <phrombutr@gmail.com>
2019-04-25 11:37:51 +0700

    perform Seque Success

--- UngAuthen/ViewController.swift
+++ UngAuthen/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    For Outlet
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }   // Main Method
-
+    
+//    For Action
+    
+    
+    @IBAction func singUpButton(_ sender: UIButton) {
+        
+        print("You Click SignUp")
+        performSegue(withIdentifier: "GoRegister", sender: nil)
+        
+    }
+    
 
 }   // Main Class
 
